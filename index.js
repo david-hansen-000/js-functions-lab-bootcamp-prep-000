58c299d47da9488d4860c7060143f1b234342a62
David Hansen <david.hansen.0000@gmail.com>
2018-08-28 20:20:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  var result="Happy holidays, "+name
+  return result
+}
+
+function happyHolidayTo(holiday, name) {
+  return "Happy "+holiday+", "+name
+}
+
+function holidayCountdown(days, holiday) {
+  reuturn eval('It\'s ${days} days until ${holiday}!')
+}
